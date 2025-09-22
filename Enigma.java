f8a6fbdd348c1e2bd9fb2213d8fb7b335408f023
Jennythz <122670374+Jennythz@users.noreply.github.com>
2025-09-22 16:16:45 -0400

    Update Enigma with complete code

--- Enigma.java
+++ Enigma.java
@@ -1,35 +1,63 @@
 public class Enigma{
     //Index 0 is the current "top" character.
-    private String rotorInit[] = {"#GNUAHOVBIPWCJQXDKRYELSZFMT",
-        "#EJOTYCHMRWAFKPUZDINSXBGLQV",
-        "#BDFHJLNPRTVXZACEGIKMOQSUWY",
-        "#NWDKHGXZVRIFJBLMAOPSCYUTQE",
-        "#TGOWHLIFMCSZYRVXQABUPEJKND"};
-
+    private String rotorInit[] = {
+            "#GNUAHOVBIPWCJQXDKRYELSZFMT",
+            "#EJOTYCHMRWAFKPUZDINSXBGLQV",
+            "#BDFHJLNPRTVXZACEGIKMOQSUWY",
+            "#NWDKHGXZVRIFJBLMAOPSCYUTQE",
+            "#TGOWHLIFMCSZYRVXQABUPEJKND"};
 
     private Rotor rotors[];
-        
+
     public Enigma(int id1, int id2, int id3, String start){
-        //rotors[0] is the inner rotor
-        //rotate() will rotate inner -> middle -> outer like an odometer
         rotors = new Rotor[3];
         rotors[0] = new Rotor(rotorInit[id1-1], start.charAt(0));
         rotors[1] = new Rotor(rotorInit[id2-1], start.charAt(1));
         rotors[2] = new Rotor(rotorInit[id3-1], start.charAt(2));
-        
     }
 
-    public String decrypt(String message){        
-        //TODO
-    }
+    public String decrypt(String message){
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < message.length(); i++) {
+            char c = message.charAt(i);
 
+            // Step 1: outer -> middle
+            int pos1 = rotors[2].indexOf(c);
+            char c1 = rotors[1].charAt(pos1);
 
-    
+            // Step 2: outer -> inner
+            int pos2 = rotors[2].indexOf(c1);
+            char c2 = rotors[0].charAt(pos2);
+
+            result.append(c2);
+
+            // rotate rotors for next character
+            rotate();
+        }               // <-- 关闭 for 循环
+        return result.toString();   // <-- 别忘了返回结果
+    }               // <-- 关闭 decrypt 方法
+
     public String encrypt(String message){
-        //TODO
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < message.length(); i++) {
+            char c = message.charAt(i);
+
+            // Step 1: inner -> outer
+            int pos1 = rotors[0].indexOf(c);
+            char c1 = rotors[2].charAt(pos1);
+
+            // Step 2: middle -> outer
+            int pos2 = rotors[1].indexOf(c1);
+            char c2 = rotors[2].charAt(pos2);
+
+            result.append(c2);
+
+            // rotate rotors for next character
+            rotate();
+        }
+        return result.toString();
     }
 
-    
     private void rotate(){
         if(rotors[0].rotate()){
             if(rotors[1].rotate()){
@@ -37,5 +65,4 @@ public class Enigma{
             }
         }
     }
-    
 }
