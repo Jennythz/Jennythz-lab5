71a81c0ddf4556d2936c411ca6ce573cc7e09560
Jennythz <122670374+Jennythz@users.noreply.github.com>
2025-09-22 15:53:23 -0400

    add comments to explain Enigma code for better understanding

--- Enigma.java
+++ Enigma.java
@@ -1,5 +1,5 @@
 public class Enigma{
-
+    //Index 0 is the current "top" character.
     private String rotorInit[] = {"#GNUAHOVBIPWCJQXDKRYELSZFMT",
         "#EJOTYCHMRWAFKPUZDINSXBGLQV",
         "#BDFHJLNPRTVXZACEGIKMOQSUWY",
@@ -10,14 +10,14 @@ public class Enigma{
     private Rotor rotors[];
         
     public Enigma(int id1, int id2, int id3, String start){
-
+        //rotors[0] is the inner rotor
+        //rotate() will rotate inner -> middle -> outer like an odometer
         rotors = new Rotor[3];
         rotors[0] = new Rotor(rotorInit[id1-1], start.charAt(0));
         rotors[1] = new Rotor(rotorInit[id2-1], start.charAt(1));
         rotors[2] = new Rotor(rotorInit[id3-1], start.charAt(2));
         
     }
-
 
     public String decrypt(String message){        
         //TODO
